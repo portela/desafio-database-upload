818023134045cf0781f66ba6d782da0b89d617d5
Henrique Portela <henrique.portela@gmail.com>
2020-04-23 19:38:27 -0300

    Category - migration and model - first draft.

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,17 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('category')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column('timestamp with time zone')
   created_at: Date;
 
+  @Column('timestamp with time zone')
   updated_at: Date;
 }
 
